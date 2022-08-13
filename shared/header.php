5a032cc46488cf8dce82849f85c07fec9bd8cd17
GuilhermeZanetti <guilherme.t.zanetti@gmail.com>
2022-08-13 13:36:15 -0300

    titulo alterado

--- shared/header.php
+++ shared/header.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculadora de Magnitude - Guilherme Zanetti</title>
+    <title>Calculadora de Magnitude Online | Free</title>
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-HQHDXKY797"></script>
